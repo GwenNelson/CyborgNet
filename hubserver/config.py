2718464a1863691b15fdc95673cce03b5cea87ca
Gareth Nelson <gareth@garethnelson.com>
2015-10-17 00:49:18 +0100

    Import hubserver

---
+++ hubserver/config.py
@@ -0,0 +1,47 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2015 Gareth Nelson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+PROTOCOL_VERSION_MAJOR = 0
+PROTOCOL_VERSION_MINOR = 1
+PROTOCOL_HOSTINFO      = 'Python alpha'
+
+PROTOCOL_GREETING      = 'CyborgNet %s protocol %s.%s' % (PROTOCOL_HOSTINFO,PROTOCOL_VERSION_MAJOR,PROTOCOL_VERSION_MINOR)
+
+TCP_PORT               = 4183
+
+TCP_ENDPOINT           = 'tcp:%d' % TCP_PORT
+
+SHELL_GREETING = """
+
+Welcome to the CyborgNet hub server, type help for a list of commands
+
+"""
+
+SHELL_PROMPT = '> '
+
+SHELL_HELP = """
+ help - display this help message
+ exit - shutdown the server
+ pair - pair a module
+"""
---
+++ hubserver/protocol_handler.py
@@ -0,0 +1,109 @@
+import hmac
+import hashlib
+import Queue
+import time
+from twisted.internet import reactor, protocol, endpoints
+from twisted.protocols import basic
+from enum import Enum
+
+protocol_ver      = '0.1'
+protocol_hostinfo = 'hubserver.py alpha'
+
+
+class ConnectionState(Enum):
+   VERSION_NEGOTIATION = 1
+   PUBLIC_PRIV_SELECT  = 2
+   PRIV_ID_REQUESTED   = 3
+   PRIV_CHALLENGE_SENT = 4
+   AWAITING_COMMAND    = 5
+   EVENT_FEED_PUBLISH  = 6
+   BINARY_PUBLISH      = 7
+   FEED_SUBSCRIBE      = 8
+
+def gen_ver_greeting():
+    return 'CyborgNet %s protocol %s' % (protocol_hostinfo,protocol_ver)
+
+def gen_challenge_str():
+    return hashlib.md5(time.ctime()).hexdigest()
+
+def get_secret(module_id):
+    return ''
+
+def verify_hmac(remote_hmac, secret, challenge):
+    return True
+
+class CyborgNetProtocol(basic.LineReceiver):
+    def __init__(self, factory):
+        self.factory        = factory
+        self.state          = None
+        self.challenge      = ""
+        self.secret         = ""
+        self.module_id      = ""
+        self.private_access = False
+
+    def connectionMade(self):
+        self.sendLine(gen_ver_greeting())
+        self.state = ConnectionState.VERSION_NEGOTIATION
+
+    def connectionLost(self, reason):
+        self.state = None
+
+    def lineReceived(self, line):
+        if self.state is ConnectionState.VERSION_NEGOTIATION:
+           if line != protocol_ver:
+              self.sendLine("ERROR: Protocol mismatch")
+              self.transport.loseConnection()
+              self.state = None
+           else:
+              self.sendLine("OK protocol version %s" % protocol_ver)
+              self.sendLine("pub/priv?")
+              self.state = ConnectionState.PUBLIC_PRIV_SELECT
+
+        elif self.state is ConnectionState.PUBLIC_PRIV_SELECT:
+           if line=='pub':
+              self.sendLine("OK public access")
+              self.state = ConnectionState.AWAITING_COMMAND
+           elif line=='priv':
+              self.sendLine("ID?")
+              self.state = ConnectionState.PRIV_ID_REQUESTED
+           else:
+              self.sendLine("ERROR: invalid input")
+              self.transport.loseConnection()
+              self.state = None
+        
+        elif self.state is ConnectionState.PRIV_ID_REQUESTED:
+           self.module_id = line
+           self.secret    = get_secret(self.module_id)
+           self.challenge = gen_challenge_str()
+           self.sendLine(self.challenge)
+           self.sendLine("HMAC?")
+           self.state = ConnectionState.PRIV_CHALLENGE_SENT
+
+        elif self.state is ConnectionState.PRIV_CHALLENGE_SENT:
+           if verify_hmac(line, self.secret, self.challenge):
+              self.sendLine("OK private access")
+              self.private_access = True
+              self.state = ConnectionState.AWAITING_COMMAND
+           else:
+              self.sendLine("ERROR: Authentication error")
+              self.transport.loseConnection()
+              self.state = None
+
+        elif self.state is ConnectionState.AWAITING_COMMAND:
+           pass
+
+class CyborgNetProtocolFactory(protocol.Factory):
+    def __init__(self):
+        self.clients = set()
+
+    def buildProtocol(self, addr):
+        return CyborgNetProtocol(self)
+
+endpoints.serverFromString(reactor, "tcp:4183").listen(CyborgNetProtocolFactory())
+reactor.run()
+
+feed_public           = {} # maps feed IDs to boolean indicating whether or not the feed is public
+feed_publisher        = {} # maps feed IDs to the module ID that publishes it
+feed_subscribers      = {} # maps feed IDs to subscriber endpoints
+paired_module_secrets = {} # maps paired module IDs to module secrets
+
